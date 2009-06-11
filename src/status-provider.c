01df160c30f98b6833f0e2341edf9ab94b95d801
Ted Gould <ted@canonical.com>
2009-06-11 13:43:16 -0500

    Don't need a private or dispose or finalize.  This class is more of an interface.

--- src/status-provider.c
+++ src/status-provider.c
@@ -4,17 +4,8 @@
 
 #include "status-provider.h"
 
-typedef struct _StatusProviderPrivate StatusProviderPrivate;
-struct _StatusProviderPrivate {
-};
-
-#define STATUS_PROVIDER_GET_PRIVATE(o) \
-(G_TYPE_INSTANCE_GET_PRIVATE ((o), STATUS_PROVIDER_TYPE, StatusProviderPrivate))
-
 static void status_provider_class_init (StatusProviderClass *klass);
 static void status_provider_init       (StatusProvider *self);
-static void status_provider_dispose    (GObject *object);
-static void status_provider_finalize   (GObject *object);
 
 G_DEFINE_TYPE (StatusProvider, status_provider, G_TYPE_OBJECT);
 
@@ -22,11 +13,6 @@ static void
 status_provider_class_init (StatusProviderClass *klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
-
-	g_type_class_add_private (klass, sizeof (StatusProviderPrivate));
-
-	object_class->dispose = status_provider_dispose;
-	object_class->finalize = status_provider_finalize;
 
 	return;
 }
@@ -38,18 +24,3 @@ status_provider_init (StatusProvider *self)
 	return;
 }
 
-static void
-status_provider_dispose (GObject *object)
-{
-
-	G_OBJECT_CLASS (status_provider_parent_class)->dispose (object);
-	return;
-}
-
-static void
-status_provider_finalize (GObject *object)
-{
-
-	G_OBJECT_CLASS (status_provider_parent_class)->finalize (object);
-	return;
-}

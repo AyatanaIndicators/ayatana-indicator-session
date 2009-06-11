5ad8f590bc465555aac14f8de3ca17901b8675f3
Ted Gould <ted@canonical.com>
2009-06-11 15:54:13 -0500

    Now we're setting the status provider functions of the StatusProvider interface.

--- src/status-provider-pidgin.c
+++ src/status-provider-pidgin.c
@@ -12,10 +12,15 @@ struct _StatusProviderPidginPrivate {
 #define STATUS_PROVIDER_PIDGIN_GET_PRIVATE(o) \
 (G_TYPE_INSTANCE_GET_PRIVATE ((o), STATUS_PROVIDER_PIDGIN_TYPE, StatusProviderPidginPrivate))
 
+/* Prototypes */
+/* GObject stuff */
 static void status_provider_pidgin_class_init (StatusProviderPidginClass *klass);
 static void status_provider_pidgin_init       (StatusProviderPidgin *self);
 static void status_provider_pidgin_dispose    (GObject *object);
 static void status_provider_pidgin_finalize   (GObject *object);
+/* Internal Funcs */
+static void set_status (StatusProvider * sp, StatusProviderStatus status);
+static StatusProviderStatus get_status (StatusProvider * sp);
 
 G_DEFINE_TYPE (StatusProviderPidgin, status_provider_pidgin, STATUS_PROVIDER_TYPE);
 
@@ -28,6 +33,11 @@ status_provider_pidgin_class_init (StatusProviderPidginClass *klass)
 
 	object_class->dispose = status_provider_pidgin_dispose;
 	object_class->finalize = status_provider_pidgin_finalize;
+
+	StatusProviderClass * spclass = STATUS_PROVIDER_CLASS(klass);
+
+	spclass->set_status = set_status;
+	spclass->get_status = get_status;
 
 	return;
 }
@@ -53,4 +63,18 @@ status_provider_pidgin_finalize (GObject *object)
 
 	G_OBJECT_CLASS (status_provider_pidgin_parent_class)->finalize (object);
 	return;
+}
+
+static void
+set_status (StatusProvider * sp, StatusProviderStatus status)
+{
+
+	return;
+}
+
+static StatusProviderStatus
+get_status (StatusProvider * sp)
+{
+
+	return STATUS_PROVIDER_STATUS_OFFLINE;
 }

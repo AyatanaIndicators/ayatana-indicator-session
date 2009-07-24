091902b3d9fc772cdccc7081264799a712451beb
Ted Gould <ted@canonical.com>
2009-07-24 16:19:29 -0500

    Changing to the ayatana.org namespace

--- src/dbus-shared-names.h
+++ src/dbus-shared-names.h
@@ -2,13 +2,13 @@
 #ifndef __DBUS_SHARED_NAMES_H__
 #define __DBUS_SHARED_NAMES_H__ 1
 
-#define INDICATOR_STATUS_DBUS_NAME  "com.ubuntu.indicator.status"
-#define INDICATOR_STATUS_DBUS_OBJECT "/com/ubuntu/indicator/status"
+#define INDICATOR_STATUS_DBUS_NAME  "org.ayatana.indicator.status"
+#define INDICATOR_STATUS_DBUS_OBJECT "/org/ayatana/indicator/status"
 
-#define INDICATOR_USERS_DBUS_NAME  "com.ubuntu.indicator.users"
-#define INDICATOR_USERS_DBUS_OBJECT "/com/ubuntu/indicator/users"
+#define INDICATOR_USERS_DBUS_NAME  "org.ayatana.indicator.users"
+#define INDICATOR_USERS_DBUS_OBJECT "/org/ayatana/indicator/users"
 
-#define INDICATOR_SESSION_DBUS_NAME  "com.ubuntu.indicator.session"
-#define INDICATOR_SESSION_DBUS_OBJECT "/com/ubuntu/indicator/session"
+#define INDICATOR_SESSION_DBUS_NAME  "org.ayatana.indicator.session"
+#define INDICATOR_SESSION_DBUS_OBJECT "/org/ayatana/indicator/session"
 
 #endif /* __DBUS_SHARED_NAMES_H__ */

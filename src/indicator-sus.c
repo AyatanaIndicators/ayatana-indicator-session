fe0d2f8ccb612af918e4d9783810069c64fae9fe
Ted Gould <ted@canonical.com>
2009-08-12 21:55:02 -0600

    Cleaning up the debug message.

--- src/indicator-sus.c
+++ src/indicator-sus.c
@@ -73,7 +73,7 @@ get_label (void)
 GtkImage *
 get_icon (void)
 {
-	g_debug("Changing status icon: %s", "user-offline");
+	g_debug("Changing status icon: '%s'", "user-offline");
 	status_image = GTK_IMAGE(gtk_image_new_from_icon_name("user-offline", GTK_ICON_SIZE_MENU));
 	gtk_widget_show(GTK_WIDGET(status_image));
 	return status_image;
@@ -189,7 +189,7 @@ status_icon_cb (DBusGProxy * proxy, char * icons, GError *error, gpointer userda
 	g_return_if_fail(icons != NULL);
 	g_return_if_fail(icons[0] == '\0');
 
-	g_debug("Changing status icon: %s", icons);
+	g_debug("Changing status icon: '%s'", icons);
 	gtk_image_set_from_icon_name(status_image, icons, GTK_ICON_SIZE_MENU);
 
 	return;

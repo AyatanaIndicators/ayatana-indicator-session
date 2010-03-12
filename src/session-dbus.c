931ee90269a965820091d1397e4f5ebf123720a7
Ted Gould <ted@gould.cx>
2010-03-12 13:58:39 -0600

    Adding in base objects

---
+++ src/session-dbus.c
@@ -0,0 +1,54 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "session-dbus.h"
+
+typedef struct _SessionDbusPrivate SessionDbusPrivate;
+struct _SessionDbusPrivate {
+	gint dummy;
+};
+
+#define SESSION_DBUS_GET_PRIVATE(o) \
+(G_TYPE_INSTANCE_GET_PRIVATE ((o), SESSION_DBUS_TYPE, SessionDbusPrivate))
+
+static void session_dbus_class_init (SessionDbusClass *klass);
+static void session_dbus_init       (SessionDbus *self);
+static void session_dbus_dispose    (GObject *object);
+static void session_dbus_finalize   (GObject *object);
+
+G_DEFINE_TYPE (SessionDbus, session_dbus, G_TYPE_OBJECT);
+
+static void
+session_dbus_class_init (SessionDbusClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	g_type_class_add_private (klass, sizeof (SessionDbusPrivate));
+
+	object_class->dispose = session_dbus_dispose;
+	object_class->finalize = session_dbus_finalize;
+
+	return;
+}
+
+static void
+session_dbus_init (SessionDbus *self)
+{
+}
+
+static void
+session_dbus_dispose (GObject *object)
+{
+
+	G_OBJECT_CLASS (session_dbus_parent_class)->dispose (object);
+	return;
+}
+
+static void
+session_dbus_finalize (GObject *object)
+{
+
+	G_OBJECT_CLASS (session_dbus_parent_class)->finalize (object);
+	return;
+}
---
+++ src/session-dbus.h
@@ -0,0 +1,31 @@
+#ifndef __SESSION_DBUS_H__
+#define __SESSION_DBUS_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define SESSION_DBUS_TYPE            (session_dbus_get_type ())
+#define SESSION_DBUS(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), SESSION_DBUS_TYPE, SessionDbus))
+#define SESSION_DBUS_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), SESSION_DBUS_TYPE, SessionDbusClass))
+#define IS_SESSION_DBUS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SESSION_DBUS_TYPE))
+#define IS_SESSION_DBUS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), SESSION_DBUS_TYPE))
+#define SESSION_DBUS_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), SESSION_DBUS_TYPE, SessionDbusClass))
+
+typedef struct _SessionDbus      SessionDbus;
+typedef struct _SessionDbusClass SessionDbusClass;
+
+struct _SessionDbusClass {
+	GObjectClass parent_class;
+};
+
+struct _SessionDbus {
+	GObject parent;
+};
+
+GType session_dbus_get_type (void);
+
+G_END_DECLS
+
+#endif

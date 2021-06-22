ubports/indicator-session

use ellipsis and convergence friendly "about" caption

src/service.c
@@ -453,9 +453,9 @@ static GMenuModel *
 create_admin_section (void)
 {
   GMenu * menu;
-  gchar * help_label = g_strdup_printf(_("%s Help"), get_distro_name());
+  gchar * help_label = g_strdup_printf(_("%s Help…"), get_distro_name());
   menu = g_menu_new ();
-  g_menu_append (menu, _("About This Computer"), "indicator.about");
+  g_menu_append (menu, _("About This Device…"), "indicator.about");
   g_menu_append (menu, help_label, "indicator.help");
   g_free (help_label);
 

AyatanaIndicators/ayatana-indicator-session

src/service.c
@@ -375,9 +375,9 @@ create_admin_section (void)
 {
   GMenu * menu;
   gchar * desktop_help_label = g_strdup_printf(_("%s Desktop Help"), get_desktop_name());
-  gchar * distro_help_label = g_strdup_printf(_("%s Help"), get_distro_name());
+  gchar * distro_help_label = g_strdup_printf(_("%s Help…"), get_distro_name());
   menu = g_menu_new ();
-  g_menu_append (menu, _("About This Computer"), "indicator.about");
+  g_menu_append (menu, _("About This Device…"), "indicator.about");
   g_menu_append (menu, desktop_help_label, "indicator.desktop_help");
   g_menu_append (menu, distro_help_label, "indicator.distro_help");
   g_free (desktop_help_label);